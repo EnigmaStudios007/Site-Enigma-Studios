31147b8f75f5f8ccf1df6ffa97a63644ce2e8387
Matheus M. Prado <157816651+EnigmaStudios007@users.noreply.github.com>
2024-01-29 22:52:21 -0300

    update dnv

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,5 @@ window.addEventListener('scroll', function(){
   let header = document.querySelector('#nav');
   header.classList.toggle('rolagem', window.scrollY > 0);
 });
+
+//MATHEUS E SUA CABEÇONA S2
