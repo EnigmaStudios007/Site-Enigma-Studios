6d9cb2fcd4f9d977a1edbbc49ba0650a76c915d1
Matheus M. Prado <157816651+EnigmaStudios007@users.noreply.github.com>
2024-01-31 15:27:29 -0300

    alterações

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,5 @@
+// Animação de SCROLLAR
 window.addEventListener('scroll', function(){
   let header = document.querySelector('#nav');
   header.classList.toggle('rolagem', window.scrollY > 0);
 });
-
-//MATHEUS E SUA CABEÇONA S2
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elementoSplineWatermark = document.querySelector('#');
-
-
-//   if (elementoSplineWatermark) {
-      
-//       elementoSplineWatermark.style.visibility = 'hidden';}
